a89fed81233743788efaa32c663570c5cfca6621
Dejan <dejanpavlovski5@yahoo.com>
2025-07-11 11:58:44 +0300

    created the carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,9 +27,45 @@ function setupDropdowns() {
         toggle.classList.remove("active");
         targetButton.classList.remove("active");
         targetDropdown.classList.remove("active");
-      } 
+      }
     });
   });
 }
 
 document.addEventListener("DOMContentLoaded", setupDropdowns);
+
+
+const swiper = new Swiper(".swiper", {
+ 
+
+  slidesPerView: 1,
+  spaceBetween:30,
+  breakpoints: {
+  
+    600: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+     750: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+     1100: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+     1700: {
+      slidesPerView: 4,
+      spaceBetween: 20
+    }
+  },
+  
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+   
+});
+
+
+
